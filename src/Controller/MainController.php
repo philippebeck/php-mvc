5418a4cd4b8078820c40206e47fe2fde7045e6ef
Philippe Beck <philippe@philippebeck.net>
2020-01-22 08:57:56 +0100

    Remove render method

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,9 +3,6 @@
 namespace App\Controller;
 
 use Twig\Environment;
-use Twig\Error\LoaderError;
-use Twig\Error\RuntimeError;
-use Twig\Error\SyntaxError;
 use Twig\Loader\FilesystemLoader;
 
 /**
@@ -52,19 +49,5 @@ abstract class MainController
         header('Location: ' . $this->url($page, $params));
 
         exit;
-    }
-
-    /**
-     * Renders the Views
-     * @param string $view
-     * @param array $params
-     * @return string
-     * @throws LoaderError
-     * @throws RuntimeError
-     * @throws SyntaxError
-     */
-    public function render(string $view, array $params = [])
-    {
-        return $this->twig->render($view, $params);
     }
 }
